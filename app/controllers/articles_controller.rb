be4e265f184c7dd1a592fd7462b08f32608af53c
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-11-13 15:17:48 +0800

    Added a delete article function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,6 +33,13 @@ class ArticlesController < ApplicationController
   end
 
   # delete
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
