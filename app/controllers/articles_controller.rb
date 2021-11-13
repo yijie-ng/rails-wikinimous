77d514b0d225d117eddb4950390a96ff33118812
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-11-13 14:49:43 +0800

    Created the index page and individual article pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
   # create
+
   # read
+  def show
+    @article = Article.find(params[:id])
+  end
+
   # update
   # delete
 end
